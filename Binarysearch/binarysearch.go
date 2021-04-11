76a79b5845812dd3e83a1ed78359f5a1b345db37
lixin <2690688423@qq.com>
2021-04-11 08:37:49 +0800

    update

--- Binarysearch/binarysearch.go
+++ Binarysearch/binarysearch.go
@@ -2,17 +2,16 @@ package main
 
 import "fmt"
 
-func binarysearch(a []int,v int) int {
+func BinarySearch(a []int, v int) int {
 	n := len(a)
-
 	if n == 0 {
 		return -1
 	}
 
 	low := 0
-	high := len-1
+	high := n - 1
 	for low <= high {
-		mid := (low + high) /2
+		mid := (low + high) / 2
 		if a[mid] == v {
 			return mid
 		} else if a[mid] > v {
@@ -21,4 +20,5 @@ func binarysearch(a []int,v int) int {
 			low = mid + 1
 		}
 	}
+	return -1
 }
