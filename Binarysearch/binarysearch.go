f1cb89dea882fe355f39c5c9ed31894c6049ffa3
lixin <2690688423@qq.com>
2021-05-08 21:38:57 +0800

    update

--- Binarysearch/binarysearch.go
+++ Binarysearch/binarysearch.go
@@ -8,24 +8,21 @@ func BinarySearch(a []int, v int) bool {
 	low := 0
 	high := n - 1
 	for low <= high {
-		mid := low + (high - low)/2
+		mid := low + (high-low)/2
 
 		if a[mid] == v {
 			return true
-		}else if a[mid] > v {
+		} else if a[mid] > v {
 			high = mid - 1
-		}else {
-			low  = mid + 1
+		} else {
+			low = mid + 1
 		}
 	}
-	if low == len(a) || a[low] != v {
-		return false
-	}
 
-	return true
+	return false
 }
 
 func main() {
-	simple := []int{1,2, 9, 20, 31, 45, 63, 70, 100}
-	fmt.Println(BinarySearch(simple,20))
+	simple := []int{1, 2, 9, 20, 31, 45, 63, 70, 100}
+	fmt.Println(BinarySearch(simple, 40))
 }
