45e7487f1b19259627fff45b32dfe60d0fb9d097
lixin <2690688423@qq.com>
2021-05-10 21:23:09 +0800

    update

--- Binarysearch/binarysearch.go
+++ Binarysearch/binarysearch.go
@@ -1,16 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func BinarySearch(a []int, v int) bool {
+	
 	n := len(a)
 
 	low := 0
 	high := n - 1
+	starttime := time.Now()
 	for low <= high {
 		mid := low + (high-low)/2
-
+		
 		if a[mid] == v {
+			elapsed := time.Since(starttime)
+    		fmt.Println("All time: ", elapsed)
 			return true
 		} else if a[mid] > v {
 			high = mid - 1
@@ -18,11 +25,13 @@ func BinarySearch(a []int, v int) bool {
 			low = mid + 1
 		}
 	}
+	elapsed := time.Since(starttime)
+    fmt.Println("All time: ", elapsed)
 
 	return false
 }
 
 func main() {
 	simple := []int{1, 2, 9, 20, 31, 45, 63, 70, 100}
-	fmt.Println(BinarySearch(simple, 40))
+	fmt.Println(BinarySearch(simple, 45))
 }
