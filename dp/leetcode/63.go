70ed50acec2d5b15e68cf94a509c98e11b2714f1
lixin yang <2690688423@qq.com>
2022-02-21 14:40:08 +0800

    update 63

---
+++ dp/leetcode/63.go
@@ -0,0 +1,25 @@
+package leetcode
+
+func uniquePathsWithObstacles(obstacleGrid [][]int) int {
+	m, n := len(obstacleGrid), len(obstacleGrid[0])
+	dp := make([][]int, m)
+	for i := range dp {
+		dp[i] = make([]int, n)
+	}
+
+	for i:=0;i<m && obstacleGrid[i][0] == 0;i++ {
+		dp[i][0] = 1
+	}
+	for i:=0;i<n && obstacleGrid[0][j] == 0; j++ {
+		dp[0][j] = 1
+	}
+
+	for i:=0;i<m;i++ {
+		for j:=0; j<n;j++ {
+			if obstacleGrid[i][j] == 1 {
+				dp[i][j] = dp[i-1][j] + dp[i][j-1]
+			}
+		}
+	}
+	return dp[m-1][n-1]
+}	
