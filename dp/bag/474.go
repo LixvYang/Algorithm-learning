86409c6fad81022873f0565fa39e1d22fbb594ab
lixin yang <2690688423@qq.com>
2022-02-23 22:23:40 +0800

    add bag

---
+++ dp/bag/474.go
@@ -0,0 +1,32 @@
+package bag
+
+func findMaxForm(strs []string, m int, n int) int {
+	dp := make([][]int, m+1)
+	for i, _ := range dp {
+			dp[i] = make([]int, n+1)
+	}
+
+	for i:=0; i<len(strs);i++ {
+			zeroNum, oneNum := 0, 0
+			for _, v := range strs[i] {
+					if v == '0' {
+							zeroNum++
+					}
+			}
+			oneNum = len(strs[i])-zeroNum
+			for j:= m ; j >= zeroNum;j-- {
+		for k:=n ; k >= oneNum;k-- {
+			// 推导公式
+			dp[j][k] = max(dp[j][k],dp[j-zeroNum][k-oneNum]+1)
+		}
+	}
+	}
+	return dp[m][n]
+}
+
+func max(a, b int) int {
+	if a > b {
+			return a
+	}
+	return b
+}
