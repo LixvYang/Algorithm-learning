3ec2e55c0b32db1e9dde2842f96b3f2a2fd5f1ca
lixin yang <2690688423@qq.com>
2022-03-13 10:36:03 +0800

    update

--- backtrace/leetcode/78.go
+++ backtrace/leetcode/78.go
@@ -20,3 +20,21 @@ func Dfs(temp, nums []int, start int) {
         temp = temp[:len(temp)-1]
     }
 }
+
+func subsets(nums []int) [][]int {
+    res := [][]int{}
+    backtrack(0, nums, []int{}, &res)
+    return res
+}
+
+func backtrack(startIndex int, nums []int, track []int, res *[][]int) {
+    temp:=make([]int, len(track))
+    copy(temp, track)
+    *res = append(*res, temp)
+
+    for i:=startIndex;i<len(nums);i++ {
+        track = append(track, nums[i])
+        backtrack(i+1, nums, track, res)
+        track = track[:len(track)-1]
+    } 
+}
