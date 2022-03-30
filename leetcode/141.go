8d632e4b32f948bb8310d8ace9e0ea24621e8054
lixin yang <2690688423@qq.com>
2022-03-30 21:53:52 +0800

    add 141

---
+++ leetcode/141.go
@@ -0,0 +1,14 @@
+package leetcode
+
+func hasCycle(head *ListNode) bool {
+	slow, fast := head, head
+
+	for fast != nil && fast.Next != nil {
+			fast = fast.Next.Next
+			slow = slow.Next
+			if fast == slow {
+					return true
+			}
+	}   
+	return false
+}
