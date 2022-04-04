694b32300429b8b37b533982d4d201cd386ff570
lixin yang <2690688423@qq.com>
2022-04-04 15:57:52 +0800

    update 1

---
+++ leetcode/1.go
@@ -0,0 +1,25 @@
+// 两数相加
+package leetcode 
+
+func twoSum(nums []int, target int) []int {
+	m := make(map[int]int)
+	for index, value := range nums {
+			if preIndex, ok := m[target-value]; ok {
+					return []int{preIndex, index}
+			} else {
+					m[value] = index
+			}
+	}
+	return []int{}
+}
+
+func twosum(nums []int, target int) []int {
+	for i := 0; i < len(nums); i++ {
+		for j := i; j < len(nums); j++ {
+			if nums[i] + nums[j] == target {
+				return []int{i, j}
+			}
+		}
+	}
+	return []int{}
+}
