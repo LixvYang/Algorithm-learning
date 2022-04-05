c0896f5ebe1f8eb492eeb2124163939eb0896fa0
lixin yang <2690688423@qq.com>
2022-04-05 10:20:28 +0800

    add 104

---
+++ leetcode/104.go
@@ -0,0 +1,11 @@
+// 二叉树的最大深度
+package leetcode
+
+func maxDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	left := maxDepth(root.Left)
+	right := maxDepth(root.Right)
+	return max(left, right)+1
+}
