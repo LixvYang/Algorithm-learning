52679236bd3037a2c9e01393915bc142ef6bb236
lixin yang <2690688423@qq.com>
2022-04-05 08:39:17 +0800

    add 48

--- leetcode/48.go
+++ leetcode/48.go
@@ -1,3 +1,25 @@
 // 旋转图像
 package leetcode
 
+func rotate(matrix [][]int)  {
+	n := len(matrix)
+
+	for i := 0; i < n; i++ {
+		for j := i; j < n; j++ {
+			matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+		}
+	}
+
+	for _, row := range matrix {
+		reverse(row)
+	}
+}
+
+func reverse(n []int) {
+	l, r := 0, len(n)-1
+	for l < r {
+		n[l], n[r] = n[r], n[l]
+		l++
+		r--
+	}
+}
