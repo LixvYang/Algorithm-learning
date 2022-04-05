c3b2d1694a6417871b74588ec332ce219dad8d48
lixin yang <2690688423@qq.com>
2022-04-05 09:04:26 +0800

    add 64

---
+++ leetcode/64.go
@@ -0,0 +1,25 @@
+// 最小路径和
+package leetcode
+
+func minPathSum(grid [][]int) int {
+	m, n := len(grid), len(grid[0])
+	dp := make([][]int, m)
+	for i := range dp {
+		dp[i] = make([]int, n)
+	}
+
+	for i := 0; i < m; i++ {
+		dp[i][0] = dp[i-1][0] + grid[i][0]
+	}
+
+	for i := 0; i < n; i++ {
+		dp[0][i] = dp[0][i-1] + grid[0][i]
+	}
+
+	for i:=1; i < m; i++ {
+		for j :=1; j < n; j++ {
+			dp[i][j] = min(dp[i-1][j], dp[i][j-1]) + grid[i][j]
+		}
+	}
+	return dp[m-1][n-1]
+}
