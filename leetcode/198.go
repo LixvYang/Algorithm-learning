9ab38a3d7b886cb938ab400a7ad28f5cc5b8f325
lixin yang <2690688423@qq.com>
2022-04-07 20:21:09 +0800

    add 169 198

--- leetcode/198.go
+++ leetcode/198.go
@@ -13,7 +13,7 @@ func rob(nums []int) int {
 	dp := make([]int, len(nums))
 	dp[0], dp[1] = nums[0], max(nums[0], nums[1])
 	for i := 2; i < len(nums); i++ {
-		dp[i] = max(dp[i-1], dp[i-1]+nums[i])
+		dp[i] = max(dp[i-1], dp[i-2]+nums[i])
 	}
 
 	return dp[len(nums)-1]
---
+++ leetcode/206.go
@@ -0,0 +1,13 @@
+// 反转链表
+package leetcode
+
+func reverseList(head *ListNode) *ListNode {
+	var pre *ListNode
+	for head != nil {
+		next := head.Next
+		head.Next = pre
+		pre = head
+		head = next
+	}
+	return pre
+}
