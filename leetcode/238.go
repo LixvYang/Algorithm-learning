c1ba1721338e7948bc5a8d1187cdbd5bd26e14e0
lixin yang <2690688423@qq.com>
2022-04-09 09:14:05 +0800

    add 238

--- leetcode/238.go
+++ leetcode/238.go
@@ -16,3 +16,23 @@ func productExceptSelf(nums []int) []int {
 	}
 	return res
 }
+
+func productExceptSelf2(nums []int) []int {
+	res := make([]int, len(nums))
+	for k, _ := range res {
+		res[k] = 1
+	}
+
+	left, right := 1, 1
+	for i := 0; i < len(nums); i++ {
+		res[i] *= left
+		left *= nums[i]
+	}
+
+	for j := len(nums)-1; j >= 0; j-- {
+		res[j] *= right
+		right *= nums[j]
+	}
+	fmt.Println(res)
+	return res
+}
