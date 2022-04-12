2308f565632a16511bd3c598ec787bbe665cec6e
lixin yang <2690688423@qq.com>
2022-04-12 21:04:41 +0800

    add .

--- Stack/leetcode/232.go
+++ Stack/leetcode/232.go
@@ -14,18 +14,18 @@ func Constructor() MyQueue {
 }
 
 func (this *MyQueue) Push(x int) int {
-	for len(this.back) != 0 {
-		val := this.back[len(this.back)-1]
-		this.back = this.back[:len(this.back)-1]
-		this.stack = append(this.stack, val)
-	}
+	// for len(this.back) != 0 {
+	// 	val := this.back[len(this.back)-1]
+	// 	this.back = this.back[:len(this.back)-1]
+	// 	this.stack = append(this.stack, val)
+	// }
 
 	this.stack = append(this.stack, x)
 }
 
 func (this *MyQueue) Pop() int {
 	for len(this.stack) != 0 {
-		val := this.back[len(this.stack)-1]
+		val := this.stack[len(this.stack)-1]
 		this.stack = this.stack[:len(this.stack)-1]
 		this.back = append(this.back, val)
 	}
@@ -40,18 +40,40 @@ func (this *MyQueue) Pop() int {
 }
 
 func (this *MyQueue) Peek() int {
-	for len(this.stack) != 0 {
-		val := this.stack[len(this.stack)-1]
-		this.stack = this.stack[:len(this.stack)-1]
-		this.back = append(this.back, val)
-	}
-	if len(this.back) == 0 {
-			return 0
-	}
-	val := this.back[len(this.back)-1]
-	return val
+	// for len(this.stack) != 0 {
+	// 	val := this.stack[len(this.stack)-1]
+	// 	this.stack = this.stack[:len(this.stack)-1]
+	// 	this.back = append(this.back, val)
+	// }
+	// if len(this.back) == 0 {
+	// 		return 0
+	// }
+	// val := this.back[len(this.back)-1]
+	// return val
+
+	res := this.Pop()
+	this.back = append(this.back, res)
+	return res
 }
 
 func (this *MyQueue) IsEmpty() bool {
 	return len(this.stack) == 0 && len(this.back) == 0
+}
+
+if len(this.stackB) == 0 {
+	// 如果 stackA 里也没有元素，则队列爲空返回 -1
+	if len(this.stackA) == 0 {
+			return -1
+	}
+	// 将 stackA 的所有元素转移到 stackB
+	for len(this.stackA) > 0 {
+			// 获取 stackA 最末尾元素的下标
+			index := len(this.stackA) - 1
+			// 获取 stackA 最末尾元素的值 value
+			value := this.stackA[index]
+			// 向 stackB 的末尾追加 value
+			this.stackB = append(this.stackB, value)
+			// 从 stackA 中裁剪出末尾元素
+			this.stackA = this.stackA[:index]
+	}
 }
---
+++ offer/offer_03.go
@@ -0,0 +1,28 @@
+// 数组中的重复数字
+package offer
+
+func findRepeatNumber(nums []int) int {
+	for i := 0; i < len(nums); i++ {
+			for i != nums[i] {
+					if nums[nums[i]] == nums[i] {
+							return nums[i]
+					} 
+					nums[nums[i]], nums[i] = nums[i], nums[nums[i]]
+			}
+	}
+	return -1
+}
+
+
+func findRepeatNumber(nums []int) int {
+	numsMap := make(map[int]bool, len(nums))
+	var b int
+	for i := 0; i < len(nums); i++ {
+		if numsMap[nums[i]] {
+			b = nums[i]
+			break
+		}
+		numsMap[nums[i]] = true
+	}
+	return b
+}
