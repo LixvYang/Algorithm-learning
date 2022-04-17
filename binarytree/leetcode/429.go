e92f0bcb80e4be4267519175a48b40e3396646b3
lixin yang <2690688423@qq.com>
2022-04-17 20:49:00 +0800

    add 100

--- binarytree/leetcode/429.go
+++ binarytree/leetcode/429.go
@@ -33,3 +33,47 @@ func levelOrder(root *Node) [][]int {
 	}
 	return res
 }
+
+/**
+ * Definition for a Node.
+ * type Node struct {
+ *     Val int
+ *     Left *Node
+ *     Right *Node
+ *     Next *Node
+ * }
+ */
+
+ func connect(root *Node) *Node {
+	res:=[][]*Node{}
+    if root == nil {
+        return root
+    }
+    queue := make([]*Node, 0)
+	queue = append(queue, root)
+	for len(queue) > 0 {
+		length := len(queue)
+		tmpArr := make([]*Node, 0)
+		for i := 0; i < length; i++ {
+			node := queue[0]
+			queue = queue[1:]
+			tmpArr = append(tmpArr, node)
+
+			if node.Left != nil {
+				queue = append(queue, node.Left)
+			}
+
+			if node.Right != nil {
+				queue = append(queue, node.Right)
+			}
+		}
+		res = append(res, tmpArr)
+	}
+
+    for i := 0; i < len(res); i++ {
+        for j := 0; j < len(res[i])-1; j++ {
+            res[i][j].Next = res[i][j+1]
+        }
+    }
+    return root
+}
