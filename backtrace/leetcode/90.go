61bacf0a80488dfc7f7dac32c882c5813d433335
lixin yang <2690688423@qq.com>
2022-04-20 10:50:57 +0800

    add 90

--- backtrace/leetcode/90.go
+++ backtrace/leetcode/90.go
@@ -4,7 +4,7 @@ package leetcode
 var res[][]int
 func subsetsWithDup(nums []int)[][]int {
 	res=make([][]int,0)
-	 sort.Ints(nums)
+	quickSort(nums)
 	dfs([]int{},nums,0)
 	return res
 }
@@ -21,4 +21,33 @@ func dfs(temp, num []int, start int)  {
 		dfs(temp,num,i+1)
 		temp=temp[:len(temp)-1]
 	}
+}
+
+
+func quickSort(nums []int) {
+	separateSort(nums, 0, len(nums)-1)
+}
+
+func separateSort(nums []int, start, end int) {
+	if start > end {
+			return
+	}
+
+	i := partition(nums, start, end)
+	separateSort(nums, start, i-1)
+	separateSort(nums, i+1, end)
+}
+
+func partition(nums []int, start, end int) int {
+	pivot := nums[end]
+	
+	i := start
+	for j := start; j < len(nums); j++ {
+			if nums[j] < pivot {
+					nums[i], nums[j] = nums[j], nums[i]
+					i++
+			}
+	}
+	nums[end], nums[i] = nums[i], nums[end]
+	return i
 }
