6e601dd75e459e084d76256bcef701e732deb8c0
lixin yang <2690688423@qq.com>
2022-04-21 15:40:21 +0800

    add .

--- dp/bag/0-1bag.go
+++ dp/bag/0-1bag.go
@@ -34,3 +34,29 @@ func main() {
 	value := []int{15,20,30}
 	println(test_2_wei_bag_problem1(weight,value,4))
 }
+
+func bag() {
+	dp := make([][]int, len(weight))
+	for i := range dp {
+		dp[i] = make([]int, bagweight+1)
+	}	
+
+	for i := 0; i < len(weight); i++ {
+		dp[i][0] = 0
+	}
+
+	for i := weight[0]; i <= bagweight; i++ {
+		dp[0][i] = weight[0]
+	}
+
+	for i := 1; i < len(weight); i++ {
+		for j := 1; j <= bagweight; j++ {
+			if j < weight[i] {
+				dp[i][j] = dp[i-1][j]
+			} else {
+				dp[i][j] = max(dp[i-1][j], dp[i-1][j-weight[i]]+weight[i])
+			}
+		}
+	}
+	return dp[len(weight)-1][bagweight]
+}
