8a3d516145713e277e732be12eed1c8eba98c936
lixin yang <2690688423@qq.com>
2022-05-24 08:18:11 +0800

    update

--- binarytree/leetcode/257.go
+++ binarytree/leetcode/257.go
@@ -22,3 +22,24 @@ func binaryTreePaths(root *TreeNode) []string {
 	travel(root, "")
 	return res
 }
+
+func binaryTreePaths(root *TreeNode) []string {
+	res := make([]string, 0)
+
+	var travel func(node *TreeNode, s string)
+	travel = func(node *TreeNode, s string) {
+		if node == nil {
+			return
+		}
+		if node.Left == nil && node.Right == nil {
+			tmp := s+strconv.Itoa(node.Val)
+			res = append(res, tmp)
+			return
+		}
+		s += strconv.Itoa(node.Val) + "->"
+		travel(node.Left, s)
+		travel(node.Right, s)
+	}
+	travel(root, "")
+	return res
+}
