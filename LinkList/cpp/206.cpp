ed8b0a11645e4740cfb6af0d1c1fc00e900b500a
lixin <2690688423@qq.com>
2022-08-21 21:36:11 +0800

    update

--- LinkList/cpp/206.cpp
+++ LinkList/cpp/206.cpp
@@ -29,3 +29,28 @@ public:
     return pre;
   }
 };
+
+// 递归
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class SolutionTwo {
+public:
+    ListNode* reverseList(ListNode* head) {
+        return reverse(nullptr, head);
+    }
+
+    ListNode* reverse(ListNode* pre, ListNode* cur) {
+      if (cur == nullptr) return pre;
+      ListNode* tmp = cur->next;
+      cur->next = pre;
+      return reverse(cur, tmp);
+    }
+};
